95ce8097ce4d3240d5d0c8b59af0211ef67d5b10
Alzo02 <alzo02@icloud.com>
2018-03-23 10:50:36 +0100

    First packages

---
+++ projektlaravel/ourURL/src/ourUrlController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Projektlaravel\ourURL;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class ourUrlController extends Controller
+{
+    //
+}
